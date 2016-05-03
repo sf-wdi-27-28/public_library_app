df016835487595a646ca43dd7ff1fbe596564a9e
ruhsam <ruhsam@gmail.com>
2016-05-03 15:38:50 -0700

    enabled bcrypt

---
+++ lib_app/app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+   def index
+      @users = User.all
+      render :index
+   end
+   def new
+      @user = User.new
+      render :new
+   end
+   def create
+      user_params = params.require(:user).permit(:first_name, :last_name, :email, :password)
+      @user = User.create(user_params)
+
+      redirect_to root_path
+   end
+end
