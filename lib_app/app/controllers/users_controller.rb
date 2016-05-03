c77c1ebac1a4f4bb04853d0fd14d2613c99c78fc
PJC-1 <jumpzine@gmail.com>
2016-05-03 14:27:49 -0700

    show page is now up

--- lib_app/app/controllers/users_controller.rb
+++ lib_app/app/controllers/users_controller.rb
@@ -8,4 +8,21 @@ class UsersController < ApplicationController
     @user = User.new
     render :new
   end
+
+  def show
+    @user = User.find_by_id(params[:id])
+    render :show
+  end
+
+  def create
+    @user = User.create(user_params)
+    redirect_to root_path
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
+  end
 end
--- lib_app/config/routes.rb
+++ lib_app/config/routes.rb
@@ -3,11 +3,13 @@ Rails.application.routes.draw do
   get "/users", to: "users#index", as: "users"
   get "/users/new", to: "users#new", as: "new_user"
   post "/users", to: "users#create"
+  get "/users/:id", to: "users#show", as: "user"
 end
 
-
+# 
 # Prefix Verb URI Pattern          Controller#Action
 #   root GET  /                    users#index
 #  users GET  /users(.:format)     users#index
 # new_user GET  /users/new(.:format) users#new
 #        POST /users(.:format)     users#create
+#   user GET  /users/:id(.:format) users#show
