534e5d344085936dc925e51b85c69f5ed3b09216
ilterates <iltates@gmail.com>
2016-05-03 14:09:00 -0700

    adding user now works, starting sessions

---
+++ lib_app/app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find_by_id(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    user_params = params.require(:user).permit(:first_name, :last_name, :email, :password)
+    user = User.create(user_params)
+
+    redirect_to "/"
+  end
+  
+end
