fdc8b03528874478bfc9961853c6a0db97d0d49d
Jonathan <jortiz@tunein.com>
2016-05-03 14:17:29 -0700

    user login working

--- lib_app/db/seeds.rb
+++ lib_app/db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.Create({first_name: "Jonathan", last_name: "Ortiz" email: "Ortiz6499@gmail.com", password_digest: "test"})
