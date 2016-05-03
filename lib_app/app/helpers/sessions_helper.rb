d5501aa69700fd24470eb2383d89389ad7cb578a
ilterates <iltates@gmail.com>
2016-05-03 15:35:23 -0700

    done with part 1 run 1

--- lib_app/app/helpers/sessions_helper.rb
+++ lib_app/app/helpers/sessions_helper.rb
@@ -7,7 +7,6 @@ module SessionsHelper
 
   def current_user
     @current_user ||= User.find_by_id(session[:user_id])
-    @@very ||= User.find_by_id(session[:user_id])
   end
 
   def logged_in?
--- lib_app/app/models/user.rb
+++ lib_app/app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+  validates :email,
+    presence: true,
+    uniqueness: true
+
   def self.confirm(params)
     @user = User.find_by({email: params[:email]})
     @user.try(:authenticate, params[:password])
