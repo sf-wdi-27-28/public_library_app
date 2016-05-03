ed249242b1784bfa16e3100bea7f1185b808f949
jabad28 <abad.joanne@yahoo.com>
2016-05-03 14:48:45 -0700

    sessions create route and sessions controller

--- lib_app/app/models/user.rb
+++ lib_app/app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  def self.confirm
+    @user = User.find_by_id({email: params[:email]})
+    @user.try(:authenticate, params[:password])
+  end
 end
