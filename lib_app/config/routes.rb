32da83651f7228f5e669231fb1428da63cc3aff4
jabad28 <abad.joanne@yahoo.com>
2016-05-03 14:09:20 -0700

    create user in controllers

--- lib_app/config/routes.rb
+++ lib_app/config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get "/users", to: "users#index", as: "users"
 
   get "/users/new", to: "users#new", as: "new_user"
+
+  post "/users", to: "users#create"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
