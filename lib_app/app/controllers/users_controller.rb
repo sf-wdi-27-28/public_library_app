83d3ec63bd3f2a09e396f20ecef6727ae4c5039a
andreaainara <andrea.ainara10@gmail.com>
2016-05-03 15:42:16 -0700

    db, migrate, user_controller, users new and index, routes

--- lib_app/app/controllers/users_controller.rb
+++ lib_app/app/controllers/users_controller.rb
@@ -1,12 +1,10 @@
 class UsersController < ApplicationController
-  # grab the users
   def index
     @users = User.all
     render :index
   end
 
   def new
-    # we need to make a new user to pass the for later
     @user = User.new
     render :new
   end
@@ -14,13 +12,8 @@ class UsersController < ApplicationController
   def create
     user_params = params.require(:user).permit(:first_name, :last_name, :email, :password)
     @user = User.create(user_params)
-    login(@user)
-    redirect_to @user
-  end
 
-  def show
-    @user = User.find_by_id(params[:id])
-    render :show
+    redirect_to root_path
   end
 
 end
