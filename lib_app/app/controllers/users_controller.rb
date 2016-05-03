0d9f82732023c05ffc299dc6c42ef9119a9d124a
altonelli <altonelli14@gmail.com>
2016-05-03 14:02:44 -0700

    Sign up for user up. user show and index up

--- lib_app/app/controllers/users_controller.rb
+++ lib_app/app/controllers/users_controller.rb
@@ -1,6 +1,19 @@
 class UsersController < ApplicationController
 
+  def new
+    @user = User.new
+  end
+
   def index
     @users = User.all
+  end
+
+  def create
+    user_params = params.require(:user).permit(:first_name,:last_name,:email,:password)
+    @user = User.create(user_params)
+    redirect_to root_path
+  end
+  def show
+    @user = User.find_by_id(params[:id])
   end
 end
--- lib_app/config/routes.rb
+++ lib_app/config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
   root to: "users#index"
 
+  get "/users", to: "users#index", as: "users"
+
+  get "/signup", to: "users#new", as: "signup"
+  post "/users", to: "users#create"
+  get "/users/:id", to: "users#show", as: "user"
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
