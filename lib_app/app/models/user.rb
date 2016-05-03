f31f7ab79540ff375f416a0181a74cefac8d5cff
andreaainara <andrea.ainara10@gmail.com>
2016-05-03 14:49:09 -0700

    new sessions

--- lib_app/app/models/user.rb
+++ lib_app/app/models/user.rb
@@ -2,8 +2,8 @@ class User < ActiveRecord::Base
   has_secure_password
 
   def self.confirm(params)
-    @user = User.find_by({email: params[:email]})
-    @user.try(:authenticate, params[:params])
+  @user = User.find_by({email: params[:email]})
+  @user.try(:authenticate, params[:password])
   end
 
 end
--- lib_app/config/routes.rb
+++ lib_app/config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   post "/users", to: "users#create"
   get "/users/:id", to: "users#show", as: "user"
   get "/login", to: "sessions#new"
+  post "/sessions", to: "sessions#create"
 end
