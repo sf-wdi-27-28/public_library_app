a6051ab22e9107e0de90d467952bc68c42338c60
YOUR_GITHUB_USERNAME <jacquelynfambrough@gmail.com>
2016-05-04 00:46:50 -0700

    added error and success notices for logging in/out and invalid usernames/passwords. also added html flash notices that will render messages on page

--- lib_app/app/controllers/sessions_controller.rb
+++ lib_app/app/controllers/sessions_controller.rb
@@ -8,17 +8,20 @@ class SessionsController < ApplicationController
     @user = User.confirm(user_params)
     if @user
       login(@user)
+      flash[:notice] = "Logged in"
       redirect_to @user
     else
+      flash[:error] = "Incorrect email or password"
       redirect_to login_path
     end
   end
 
   def destroy
-    logout
+    session[:user_id] = nil
+    flash[:notice] = "Logged out"
     redirect_to root_path
   end
-  
+
   private
   def user_params
     params.require(:user).permit(:email, :password)
