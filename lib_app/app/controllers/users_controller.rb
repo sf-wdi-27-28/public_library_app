0039ddaf106b1bfff070e6c81e1c0cd81296ded7
YOUR_GITHUB_USERNAME <jacquelynfambrough@gmail.com>
2016-05-04 00:11:59 -0700

    added show method

--- lib_app/app/controllers/users_controller.rb
+++ lib_app/app/controllers/users_controller.rb
@@ -15,6 +15,11 @@ class UsersController < ApplicationController
     redirect_to root_path
   end
 
+  def show
+    @user = User.find_by_id(params[:id])
+    render :show
+  end
+
   private
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password)
