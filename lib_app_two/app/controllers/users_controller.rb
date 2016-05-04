5b89e83f8e34bfc3ec347b447623ff4253c7ac9c
PJC-1 <jumpzine@gmail.com>
2016-05-03 18:05:20 -0700

    user show success

--- lib_app_two/app/controllers/users_controller.rb
+++ lib_app_two/app/controllers/users_controller.rb
@@ -3,4 +3,27 @@ class UsersController < ApplicationController
     @users = User.all
     render :index
   end
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def show
+    @user = User.find_by_id(params[:id])
+    render :show
+  end
+
+  def create
+    @user= User.create(user_params)
+    redirect_to root_path
+  end
+
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
+  end
+
+
 end
--- lib_app_two/config/routes.rb
+++ lib_app_two/config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   root to: "users#index"
+
+  get "/users", to: "users#index", as: "users"
+  get "/users/new", to: "users#new", as: "new_user"
+  post "/users", to: "users#create"
+  get "/users/:id", to: "users#show", as: "user"
 end
+
+
+# 
+# refix Verb URI Pattern          Controller#Action
+#     root GET  /                    users#index
+#    users GET  /users(.:format)     users#index
+# new_user GET  /users/new(.:format) users#new
+#          POST /users(.:format)     users#create
+#     user GET  /users/:id(.:format) users#show
