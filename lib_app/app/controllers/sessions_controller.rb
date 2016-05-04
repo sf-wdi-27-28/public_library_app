7f754da0ccdcb4873569ffe004e4fe438105f40d
YOUR_GITHUB_USERNAME <jacquelynfambrough@gmail.com>
2016-05-04 00:33:21 -0700

    added create and user_params methods

--- lib_app/app/controllers/sessions_controller.rb
+++ lib_app/app/controllers/sessions_controller.rb
@@ -3,4 +3,19 @@ class SessionsController < ApplicationController
     @user = User.new
     render :new
   end
+
+  def create
+    @user = User.confirm(user_params)
+    if @user
+      login(@user)
+      redirect_to @user
+    else
+      redirect_to login_path
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
 end
