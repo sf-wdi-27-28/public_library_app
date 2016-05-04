72923cb58311288a6770f46a0b86e5561e264b25
PJC-1 <jumpzine@gmail.com>
2016-05-03 20:02:33 -0700

    working on login

--- lib_app_two/config/routes.rb
+++ lib_app_two/config/routes.rb
@@ -5,10 +5,12 @@ Rails.application.routes.draw do
   get "/users/new", to: "users#new", as: "new_user"
   post "/users", to: "users#create"
   get "/users/:id", to: "users#show", as: "user"
+  get "/login", to: "sessions#new"
+  post "/sessions", to: "sessions#create"
 end
 
 
-# 
+#
 # refix Verb URI Pattern          Controller#Action
 #     root GET  /                    users#index
 #    users GET  /users(.:format)     users#index
