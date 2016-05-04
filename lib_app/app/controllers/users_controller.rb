5650ec2e5f1a39fd61b041532fc072cf8341bf77
andreaainara <andrea.ainara10@gmail.com>
2016-05-03 17:44:30 -0700

    finished signin and sessions create/destroy, refactored params in user and sessions controller

--- lib_app/app/controllers/users_controller.rb
+++ lib_app/app/controllers/users_controller.rb
@@ -17,15 +17,20 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find_by_id(paramd[:id])
+    @user = User.find_by_id(params[:id])
     render :show
   end
 
   def create
-    user_params = params.require(:user).permit(:first_name, :last_name, :email, :password)
-    @user = User.create(user_params)
-    login(@user)
-    redirect_to @user
+  @user = User.create(user_params)
+  login(@user) # <-- login the user
+  redirect_to @user # <-- go to show
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
   end
 
 end
--- lib_app/config/routes.rb
+++ lib_app/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: "users#index"
 
   get "/users", to: "users#index", as: "users"
-  get "/users/new", to: "users#new", sa: "new_user"
+  get "/users/new", to: "users#new", as: "new_user"
   post "/users", to: "users#create"
   get "/users/:id", to: "users#show", as: "user"
   get "/login", to: "sessions#new"
