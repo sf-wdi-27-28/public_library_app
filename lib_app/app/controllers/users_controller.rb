f7409dabddf1d4d8493dca799b44d1614fdcb0f8
Mleon195 <mleon@mail.ccsf.edu>
2016-05-03 17:46:13 -0700

    Refactoring Params

--- lib_app/app/controllers/users_controller.rb
+++ lib_app/app/controllers/users_controller.rb
@@ -10,16 +10,24 @@ class UsersController < ApplicationController
       render :new
     end
 
+    def show
+    @user = User.find_by_id(params[:id])
+    render :show
+    end
+
+    
     def create
-        user_params = params.require(:user).permit(:first_name, :last_name, :email, :password)
         @user = User.create(user_params)
         login(@user) # <-- login the user
         redirect_to @user
     end
 
-    def show
-    @user = User.find_by_id(params[:id])
-    render :show
+    private
+
+    def user_params
+       params.require(:user).permit(:first_name, :last_name, :email, :password)
     end
+
+
 
 end
