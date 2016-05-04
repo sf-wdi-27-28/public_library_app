d1d351e19d39154b8bcbd98a516ba41d67d9e3cc
PJC-1 <jumpzine@gmail.com>
2016-05-04 11:06:38 -0700

    set up library index and new view

---
+++ lib_app_two/app/controllers/libraries_controller.rb
@@ -0,0 +1,11 @@
+class LibrariesController < ApplicationController
+  def index
+    @libraries = Library.all
+    render :index
+  end
+
+  def new
+    @library = Library.new
+    render :new
+  end
+end
--- lib_app_two/config/routes.rb
+++ lib_app_two/config/routes.rb
@@ -5,16 +5,24 @@ Rails.application.routes.draw do
   get "/users/new", to: "users#new", as: "new_user"
   post "/users", to: "users#create"
   get "/users/:id", to: "users#show", as: "user"
+
   get "/login", to: "sessions#new"
   post "/sessions", to: "sessions#create"
   get "/logout", to: "sessions#destroy"
+
+  get "/libraries", to: "libraries#index"
+  get "/libraries/new", to: "libraries#new", as: "new_library"
 end
 
 
-#
-# refix Verb URI Pattern          Controller#Action
-#     root GET  /                    users#index
-#    users GET  /users(.:format)     users#index
-# new_user GET  /users/new(.:format) users#new
-#          POST /users(.:format)     users#create
-#     user GET  /users/:id(.:format) users#show
+# Prefix Verb URI Pattern              Controller#Action
+#   root GET  /                        users#index
+#  users GET  /users(.:format)         users#index
+# new_user GET  /users/new(.:format)     users#new
+#        POST /users(.:format)         users#create
+#   user GET  /users/:id(.:format)     users#show
+#  login GET  /login(.:format)         sessions#new
+# sessions POST /sessions(.:format)      sessions#create
+# logout GET  /logout(.:format)        sessions#destroy
+# libraries GET  /libraries(.:format)     libraries#index
+# new_library GET  /libraries/new(.:format) libraries#new
