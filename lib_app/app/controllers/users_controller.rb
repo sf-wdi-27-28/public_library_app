caa0a3f9498acb72253755c48f80dde44d28dc62
Two-Watch-Boy <twowatchboy@gmail.com>
2016-05-04 09:22:11 -0700

    compleeted homework

--- lib_app/app/controllers/users_controller.rb
+++ lib_app/app/controllers/users_controller.rb
@@ -1,6 +1,34 @@
 class UsersController < ApplicationController
+
+  def new
+    # we need to make
+    # a new user
+    # to pass to the
+    # form later
+    @user = User.new
+    render :new
+  end
+
+  def show
+    @user = User.find_by_id(params[:id])
+    render :show
+  end
+
   def index
     @users = User.all
     render :index
   end
+
+  def create
+    @user = User.create(user_params) # calls user_params method
+    login(@user)
+    redirect_to @user
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
+  end
+
 end
--- lib_app/app/models/user.rb
+++ lib_app/app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+  has_secure_password
+  
+  validates :email, uniqueness: true
+
+  def self.confirm(params)
+    @user = User.find_by({email: params[:email]})
+    @user.try(:authenticate, params[:password])
+  end
+
 end
