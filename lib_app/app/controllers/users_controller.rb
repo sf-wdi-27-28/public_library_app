7cbb9a78a3c9bc9617460f3f846cb108386a2010
ruhsam <ruhsam@gmail.com>
2016-05-04 08:01:16 -0700

    sessions completed

--- lib_app/app/controllers/users_controller.rb
+++ lib_app/app/controllers/users_controller.rb
@@ -10,7 +10,11 @@ class UsersController < ApplicationController
    def create
       user_params = params.require(:user).permit(:first_name, :last_name, :email, :password)
       @user = User.create(user_params)
-
-      redirect_to root_path
+      login(@user)
+      redirect_to @user
+   end
+   def show
+      @user = User.find_by_id(params[:id])
+      render :show
    end
 end
